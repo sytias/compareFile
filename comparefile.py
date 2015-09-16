5a6c657f7f56c7f5c814fbb971a679200b94bfc8
sytias <cyc870830@gmail.com>
2015-09-16 10:59:32 -0400

    files name match patterns

--- comparefile.py
+++ comparefile.py
@@ -17,7 +17,8 @@ if(len(Filenames)>0):
 			
 			
 for origfn in DictOrig:
-	TEST_origfn = "TEST_" + origfn
+	index = origfn.find('_', 0) +1
+	TEST_origfn = origfn[0:index] + "TEST_" + origfn[index:]
 	if TEST_origfn in DictTest:
 		result = os.system('diff %s %s' %(Path+origfn, Path+TEST_origfn));
 		DictOrig["%s" %origfn] = result
@@ -27,4 +28,4 @@ for origfn in DictOrig:
 			
 
 for origfn in DictOrig:
-	print (origfn, ":" , DictOrig["%s" %origfn])
+	print (origfn + ":" + DictOrig["%s" %origfn])
